1ec4519b2a5a25fee65074ea59d1d09a48c3dddc
Hogor007 <asifpenta@gmail.com>
2022-04-05 00:20:00 +0600

    added navbar and images

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
