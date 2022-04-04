c8e68e6d1ffdf534c495e42d9476e32606739879
Hogor007 <asifpenta@gmail.com>
2022-04-05 00:27:05 +0600

    added reviews with data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Reviews from "./components/Reviews/Reviews";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Header></Header>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
       </Routes>
     </div>
   );
