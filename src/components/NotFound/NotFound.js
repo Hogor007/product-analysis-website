ff11542b80b8f6533e9c56084dc2a4a41efb5064
Hogor007 <asifpenta@gmail.com>
2022-04-05 00:36:11 +0600

    ERROR 404

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-center my-6">
+        404
+      </h1>
+      <h1 className="text-center text-4xl text-slate-700 font-serif">Where's the page, Lebowski? Where's the page?</h1>
+    </div>
+  );
+};
+
+export default NotFound;
