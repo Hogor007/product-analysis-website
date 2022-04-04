8f74c2992cc63302bc88230e7dc509308aaa065f
Hogor007 <asifpenta@gmail.com>
2022-04-05 00:40:49 +0600

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="container mx-auto p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2022 Bengal Meat™ , All Rights Reserved.
+        </span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          Made with 💖 by Hogor007
+        </span>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
